54ad4f46c9f1d8978e9df76631748a5ade4666ff
HyunsuYu <lioie6478@gmail.com>
2024-08-10 04:04:34 +0900

    Initial commit

---
+++ Assets/Scripts/Canvas/Canvas_AbstractAndBasicInputData.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+using EasyTechToolUI;
+
+
+public sealed class Canvas_AbstractAndBasicInputData : TransitionCanvasMonoBehaviour<AbstractAndBasicInputDataCanvasCommonData>
+{
+    public override void OnTransition(in string from)
+    {
+        base.OnTransition(from);
+    }
+
+    public override void Initialize()
+    {
+        base.Initialize();
+    }
+}
+
+public sealed class AbstractAndBasicInputDataCanvasCommonData
+{
+
+}
