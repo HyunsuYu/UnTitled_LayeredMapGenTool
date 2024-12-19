2b5d0dddcafba5f8417f38f948b8c9882edd9057
HyunsuYu <lioie6478@gmail.com>
2024-12-20 01:33:07 +0900

    Little Fix - Feafure : TilemapManagerTest

--- Assets/Scripts/SingleTilemapLayerTest.cs
+++ Assets/Scripts/SingleTilemapLayerTest.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:022e0914f85f64070977db88907082a67c27bc89c9903e05fd0dfae46bd6d7b2
-size 52881
+oid sha256:68392287d667225114f4388d3aaad85ce81bb4d9eaa8470a71ba5d0a4540888b
+size 53162
--- Assets/Scripts/TilemapPlayer.cs
+++ Assets/Scripts/TilemapPlayer.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0f7f069414b4a7ec3c9149dcda774aff4ca5f4c529a70b5520b3112b19fb2ab7
-size 5168
+oid sha256:b26ad54e49f46b9b1100691ab31816648919c0525313a3e3376d0efc01e22dd6
+size 5278
